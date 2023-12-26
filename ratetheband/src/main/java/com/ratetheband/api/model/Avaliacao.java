b15b667eda87bae9ec938d70429f791fd834d344
Pedro Soares <pedrowesleyfs@gmail.com>
2023-12-26 16:35:22 -0300

    criado os models para a classe avaliacao e comentario

---
+++ ratetheband/src/main/java/com/ratetheband/api/model/Avaliacao.java
@@ -0,0 +1,72 @@
+package com.ratetheband.api.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@Entity
+@Table (name= "tb_avaliacoes")
+public class Avaliacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@PositiveOrZero(message = "O valor nota precisa ser igual ou maior que zero")
+	private Long nota = 0L;
+	
+	@UpdateTimestamp
+	private LocalDateTime createdDate;
+	
+	@NotNull(message = "o campo bandaId não pode estar vazio!")
+	private Long bandaId;
+	
+//	@ManyToOne
+//	@JsonIgnoreProperties("avaliacoes")
+//	private Usuario usuario;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNota() {
+		return nota;
+	}
+
+	public void setNota(Long nota) {
+		this.nota = nota;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Long getBandaId() {
+		return bandaId;
+	}
+
+	public void setBandaId(Long bandaId) {
+		this.bandaId = bandaId;
+	}
+	
+
+	
+	
+}
---
+++ ratetheband/src/test/java/com/ratetheband/api/RatethebandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratetheband.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatethebandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
